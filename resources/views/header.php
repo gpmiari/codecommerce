84b3e05945111fd0a91ebb21850a596ec04546ae
Guilherme Miari <gpmiari@gmail.com>
2016-06-03 03:24:20 -0300

    Gerando models e testando o tinker

---
+++ resources/views/header.php
@@ -0,0 +1,13 @@
+
+<script src="https://www.gstatic.com/firebasejs/live/3.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBZ0_SWr3lHyIlIHyH8zmdRe8HqW7qnN74",
+    authDomain: "customer-fb2c0.firebaseapp.com",
+    databaseURL: "https://customer-fb2c0.firebaseio.com",
+    storageBucket: "customer-fb2c0.appspot.com",
+  };
+  firebase.initializeApp(config);
+</script>
+    

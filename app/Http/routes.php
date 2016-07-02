b609902a64bab47bf327f434f741709b69da1907
Guilherme Miari <gpmiari@gmail.com>
2016-07-02 18:30:52 -0300

    testando rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,14 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+// for use request type put add in form filde hidden with name _method and value PUT
+// route pattern to validate all routes comming with id paramters to numeric parameter
+Route::pattern('id', '[0-9]+');
 
 get("/", "UserController@index");
 get("/category", "AdminCategoriesController@index");
 get("/products", "AdminProductsController@index");
+
+Route::get("/products", ['as' => 'produtos', function() {
+    return "rotamaluca";
+}]);

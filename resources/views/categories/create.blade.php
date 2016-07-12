b4c2eb7e1f1737ffe0a277bb4c58dab7b7d81ca3
Guilherme Miari <gpmiari@gmail.com>
2016-07-12 02:36:47 -0300

    criando form

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -5,6 +5,17 @@
         <h1>Create category</h1>
 
         {!! Form::open() !!}
+        <div class="form-group">
+            {!! Form::label('name', 'Name:') !!}
+            {!! Form::text('name', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('description', 'Description:') !!}
+            {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::submit('Add', ['class' => 'btn btn-primary form-control']) !!}
+        </div>
         {!! Form::close() !!}
     </div>
 @endsection
